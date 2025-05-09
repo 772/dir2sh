0967f87d4e95f54abaf7286419b1255f1ee45615
Armin Schäfer M.Sc. <arminnessie@gmail.com>
2025-05-09 20:18:42 +0200

    Fix bug

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,42 @@
 use base64::Engine;
+use std::path::Path;
 use std::{fs, io};
-use std::path::{Path, PathBuf};
 
 fn main() -> io::Result<()> {
-    let commands = generate_commands()?;
-    
     if let Ok(mut clipboard) = arboard::Clipboard::new() {
-        clipboard.set_text(commands).unwrap();
+        clipboard.set_text(generate_commands()?).unwrap();
     }
     Ok(())
 }
 
 fn generate_commands() -> io::Result<String> {
-    let mut commands = String::new();
     let current_dir = std::env::current_dir()?;
-    
-    if let Some(dir_name) = current_dir.file_name() {
-        commands.push_str(&format!("mkdir -p \"{}\"\n", dir_name.to_string_lossy()));
-    }
-    
-    process_directory(&current_dir, &mut commands)?;
+    let dir_name = current_dir.file_name().unwrap().to_string_lossy();
+    let mut commands = format!("mkdir -p \"{dir_name}\"\n");
+    process_directory(&current_dir, &current_dir, &mut commands)?;
     Ok(commands)
 }
 
-fn process_directory(dir: &PathBuf, commands: &mut String) -> io::Result<()> {
-    for entry in fs::read_dir(dir)? {
+fn process_directory(base_dir: &Path, current_dir: &Path, commands: &mut String) -> io::Result<()> {
+    for entry in fs::read_dir(current_dir)? {
         let entry = entry?;
         let path = entry.path();
-        let relative_path = path.strip_prefix(dir.parent().unwrap_or(dir)).unwrap();
-        let target_path = relative_path.to_string_lossy().replace("\\", "/");
-
+        let relative_path = path.strip_prefix(base_dir).unwrap();
+        let target_path = format!(
+            "{}/{}",
+            base_dir.file_name().unwrap().to_string_lossy(),
+            relative_path.display().to_string().replace("\\", "/")
+        );
         if path.is_dir() {
-            commands.push_str(&format!("mkdir -p \"{}\"\n", target_path));
-            process_directory(&path, commands)?;
+            commands.push_str(&format!("mkdir -p \"{target_path}\"\n"));
+            process_directory(base_dir, &path, commands)?;
         } else if path.is_file() {
-            let content = fs::read(&path)?;
-            let encoded = base64::engine::general_purpose::STANDARD.encode(&content);
+            let encoded = base64::engine::general_purpose::STANDARD.encode(fs::read(&path)?);
             commands.push_str(&format!(
-                "printf '%s' '{}' | base64 -d > \"{}\"\n",
-                encoded, target_path
+                "printf '%s' '{encoded}' | base64 -d > \"{target_path}\"\n"
             ));
         }
     }
     Ok(())
 }
+
